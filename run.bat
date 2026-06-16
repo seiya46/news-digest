f3c4af3ac56adde622b0e840b4490e59bd9358f6
seiya46 <seiya46@users.noreply.github.com>
2026-06-16 14:20:00 +0900

    run.bat: GitHubへの自動push機能を追加

--- run.bat
+++ run.bat
@@ -1,7 +1,11 @@
 @echo off
 cd /d "%~dp0"
+set PATH=%PATH%;C:\Program Files\Git\cmd
 py fetch_news.py
 if exist index.html (
     start "" "index.html"
 )
+git add index.html reports
+git commit -m "Update news digest"
+git push
 pause
